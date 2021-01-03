877069a015f972ee9e77149732a01d82aac42066
ShubhamRanaDev <shubhamrana.dev@gmail.com>
2021-01-03 02:48:36 -0500

    add post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,40 @@
 const express = require('express')
 const router = express.Router();
+const {check, validationResult} = require('express-validator')
+const auth = require('../../middleware/auth')
+const Post = require('../../models/Post')
+const User = require('../../models/User')
+const Profile = require('../../models/Profile')
 
-router.get('/',(req,res)=>res.send('User Route'))
+//post req for posts
+router.post('/', [auth, [
+        check('text', 'Text is Required')
+            .not()
+            .isEmpty()
+    ]],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()})
+        }
 
-module.exports=router;
+
+        try {
+            const user = await User.findById(req.user.id).select('-password')
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            })
+
+            const post = await newPost.save()
+            res.json(post)
+
+        } catch (e) {
+            console.error(e.message)
+            res.status(500).send('server error')
+        }
+    })
+
+module.exports = router;
